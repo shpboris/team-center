1628781a5ce43b98f2a047b25a64beb3d2c78f3a
unknown <shpilb@ISENSHPILBL1C.corp.emc.com>
2015-04-06 22:15:52 +0300

    Dummy commit to merge changes

--- src/main/java/org/team/Application.java
+++ src/main/java/org/team/Application.java
@@ -1,9 +1,15 @@
 package org.team;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan
 public class Application {
 
     public static void main(String[] args) {
